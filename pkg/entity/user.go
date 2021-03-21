08c39aca5b6485cafd2b2315d0fe6fdfb94f7f5a
tanimutomo <tanimutomo@gmail.com>
2021-03-21 20:12:36 +0900

    feat(user): Add unique constraint to user.Name

--- pkg/entity/user.go
+++ pkg/entity/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	ID        int64     `gorm:"column:id;primarykey;autoincrement"`
-	Name      string    `gorm:"column:name"`
+	Name      string    `gorm:"column:name;unique"`
 	CreatedAt time.Time `gorm:"column:created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at"`
 }
